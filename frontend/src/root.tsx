4170ff04ef9561697cbd3593e96904ce865a10cc
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-06-15 14:15:49 -0700

    Move to `less`

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -2,7 +2,7 @@ import { component$, useServerData } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
 import { RouterHead } from './components/router-head/router-head';
 
-import './global.css';
+import './global.less';
 
 export default component$(() => {
 	const nonce = useServerData<string | undefined>('nonce');
