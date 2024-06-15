a9fd1ec14b6c1a1d6d4bd525723c33d903040203
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-06-15 14:01:33 -0700

    Enable CSP

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -1,32 +1,23 @@
-import { component$ } from "@builder.io/qwik";
-import {
-  QwikCityProvider,
-  RouterOutlet,
-  ServiceWorkerRegister,
-} from "@builder.io/qwik-city";
-import { RouterHead } from "./components/router-head/router-head";
+import { component$, useServerData } from '@builder.io/qwik';
+import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+import { RouterHead } from './components/router-head/router-head';
 
-import "./global.css";
+import './global.css';
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Don't remove the `<head>` and `<body>` elements.
-   */
+	const nonce = useServerData<string | undefined>('nonce');
 
-  return (
-    <QwikCityProvider>
-      <head>
-        <meta charset="utf-8" />
-        <link rel="manifest" href="/manifest.json" />
-        <RouterHead />
-      </head>
-      <body lang="en">
-        <RouterOutlet />
-        <ServiceWorkerRegister />
-      </body>
-    </QwikCityProvider>
-  );
+	return (
+		<QwikCityProvider>
+			<head>
+				<meta charset="utf-8" />
+				<link rel="manifest" href="/manifest.json" />
+				<RouterHead />
+			</head>
+			<body lang="en">
+				<RouterOutlet />
+				<ServiceWorkerRegister nonce={nonce} />
+			</body>
+		</QwikCityProvider>
+	);
 });
