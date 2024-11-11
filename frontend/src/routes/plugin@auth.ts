8428f1be7d4a8d8bde8a7df4f6cf18f39fb5a6fd
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-11-11 10:02:51 -0700

    Fix auth

--- frontend/src/routes/plugin@auth.ts
+++ frontend/src/routes/plugin@auth.ts
@@ -1,26 +1,53 @@
-import Passkey from '@auth/core/providers/passkey';
 import { D1Adapter } from '@auth/d1-adapter';
-import { serverAuth$ } from '@builder.io/qwik-auth';
+import { QwikAuth$ } from '@auth/qwik';
+import Passkey from '@auth/qwik/providers/passkey';
+import { DEFAULT_WEBAUTHN_TIMEOUT } from '@auth/qwik/providers/webauthn';
 
-export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } = serverAuth$(({ platform }) => ({
-	adapter: D1Adapter(platform.env.DB),
-	providers: [Passkey],
-	experimental: {
-		enableWebAuthn: true,
-	},
-	logger: {
-		// Only debug log if not production
-		debug: platform.env.NODE_ENV !== 'production' ? console.debug : undefined,
-		// Keep the following to use nice console log separation
-		warn: console.warn,
-		error: console.error,
-	},
-	secret: platform.env.AUTH_SECRET,
-	session: {
-		generateSessionToken: crypto.randomUUID,
-		// seconds * minutes * hours * days
-		maxAge: 60 * 60 * 24 * 14,
-	},
-	trustHost: true,
-	useSecureCookies: true,
-}));
+export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(({ platform }) => {
+	const { cf } = platform.request;
+
+	return {
+		adapter: D1Adapter(platform.env.DB),
+		providers: [
+			Passkey({
+				id: 'passkey',
+				name: 'Passkey',
+				authenticationOptions: {
+					timeout: DEFAULT_WEBAUTHN_TIMEOUT,
+					userVerification: 'preferred',
+				},
+				registrationOptions: {
+					timeout: DEFAULT_WEBAUTHN_TIMEOUT,
+					authenticatorSelection: {
+						residentKey: 'preferred',
+						userVerification: 'preferred',
+					},
+				},
+				verifyAuthenticationOptions: {
+					requireUserVerification: true,
+				},
+				verifyRegistrationOptions: {
+					requireUserVerification: true,
+				},
+			}),
+		],
+		experimental: {
+			enableWebAuthn: true,
+		},
+		debug: platform.env.NODE_ENV !== 'production',
+		logger: {
+			// Only debug log if not production
+			debug: platform.env.NODE_ENV !== 'production' ? console.debug : undefined,
+			// Keep the following to use nice console log separation
+			warn: console.warn,
+			error: console.error,
+		},
+		secret: platform.env.AUTH_SECRET,
+		session: {
+			strategy: 'database',
+			// seconds * minutes * hours * days
+			maxAge: 60 * 60 * 24 * 14,
+		},
+		trustHost: true,
+	};
+});
