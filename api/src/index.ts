c78119a7ab93ffd4aaf7825a7a5d3320bf7c23dd
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-12-17 18:45:45 -0700

    copy pasta

--- api/src/index.ts
+++ api/src/index.ts
@@ -13,7 +13,7 @@ import type { ContextVariables, EnvVars } from '~/types.mjs';
 import { DBManager, StaticDatabase } from '~shared/db-core/db.mjs';
 import { Helpers } from '~shared/helpers/index.mjs';
 
-export { DataKeyRotation as PullSources } from '../../wf/dataKeyRotation.mjs';
+export { DataKeyRotation } from '../../wf/dataKeyRotation.mjs';
 
 export default class extends WorkerEntrypoint<EnvVars> {
 	override async fetch(request: Request) {
