f05a4167b868c16ae4b4ccd37eb447bc2d4a77c6
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-03-04 11:42:18 -0700

    Update injected node modules

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ import type { EnvVars } from '~/types';
 /**
  * @link https://developers.cloudflare.com/workers/runtime-apis/nodejs
  */
-const cloudflareNodeRuntimes: `node:${string}`[] = ['node:assert', 'node:async_hooks', 'node:buffer', 'node:crypto', 'node:diagnostics_channel', 'node:events', 'node:path', 'node:process', 'node:stream', 'node:string_decoder', 'node:test', 'node:url', 'node:util', 'node:zlib'];
+const cloudflareNodeRuntimes: `node:${string}`[] = ['node:assert', 'node:async_hooks', 'node:buffer', 'node:crypto', 'node:diagnostics_channel', 'node:dns', 'node:events', 'node:net', 'node:path', 'node:process', 'node:stream', 'node:string_decoder', 'node:test', 'node:timers', 'node:url', 'node:util', 'node:zlib'];
 /**
  * @link https://workers-types.pages.dev/
  */
