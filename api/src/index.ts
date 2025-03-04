33e33c7b40c44e98b866f1ec84fda191e26d1196
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-03-04 12:29:51 -0700

    Even more optimization

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,53 +5,83 @@ export { DataKeyRotation } from '../../wf/dataKeyRotation.mjs';
 
 export default class extends WorkerEntrypoint<EnvVars> {
 	override async fetch(request: Request) {
-		return Promise.all([import('hono'), import('@hono/zod-validator'), import('zod'), import('~/base.mjs'), import('~/docs.mjs')]).then(([{ Hono }, { zValidator }, { z }, { default: baseApp }, { default: docs }]) => {
-			const app = new Hono<{ Bindings: EnvVars; Variables: ContextVariables }>();
-			const secondaryRequest = request.clone();
+		const secondaryRequest = request.clone();
+		const app = await import('hono').then(({ Hono }) => new Hono<{ Bindings: EnvVars; Variables: ContextVariables }>());
 
-			// Dev debug injection point
-			app.use('*', async (c, next) => {
-				if (c.env.NODE_ENV === 'development') {
+		// Dev debug injection point
+		app.use('*', async (c, next) => {
+			if (c.env.NODE_ENV === 'development') {
+			}
+
+			await next();
+		});
+
+		// Security
+		app.use('*', (c, next) => import('hono/csrf').then(({ csrf }) => csrf()(c, next)));
+		// Allow only GET on documentation site
+		// app.use('*/docs', (c, next) =>
+		// 	import('hono/cors').then(({ cors }) =>
+		// 		cors({
+		// 			origin: '*',
+		// 			allowMethods: ['POST', 'OPTIONS'],
+		// 			maxAge: 300,
+		// 		})(c, next),
+		// 	),
+		// );
+		app.use('*', (c, next) =>
+			import('hono/cors').then(({ cors }) =>
+				cors({
+					origin: '*',
+					allowMethods: ['GET', 'OPTIONS'],
+					maxAge: 300,
+				})(c, next),
+			),
+		);
+
+		// Performance
+		app.use('*', (c, next) => import('hono/etag').then(({ etag }) => etag()(c, next)));
+
+		// Debug
+		app.use('*', (c, next) => import('hono/timing').then(({ timing }) => timing()(c, next)));
+		app.use('*', async (c, next) => {
+			if (c.env.NODE_ENV === 'development') {
+				return import('hono/logger').then(({ logger }) => logger()(c, next));
+			}
+
+			await next();
+		});
+
+		// Variable Setup
+		app.use('*', async (c, next) => {
+			c.set('bodyClone', secondaryRequest);
+
+			await next();
+		});
+		app.use('*', async (c, next) =>
+			Promise.all([import('~shared/helpers/index.mjs'), import('~shared/db-core/db.mjs')]).then(async ([{ Helpers }, { DBManager }]) => {
+				if (Helpers.isLocal(c.env.CF_VERSION_METADATA)) {
+					await import('~shared/db-core/db.mjs').then(({ StaticDatabase }) =>
+						c.set(
+							'r_db',
+							DBManager.getDrizzle(
+								{
+									accountId: c.env.CF_ACCOUNT_ID,
+									apiToken: c.env.CF_API_TOKEN,
+									databaseId: c.env.ENVIRONMENT === 'production' ? StaticDatabase.Root.eaas_root : StaticDatabase.Root.eaas_root_p,
+								},
+								c.env.NODE_ENV !== 'production',
+							),
+						),
+					);
+				} else {
+					c.set('r_db', DBManager.getDrizzle(c.env.EAAS_ROOT, c.env.NODE_ENV !== 'production'));
 				}
 
 				await next();
-			});
+			}),
+		);
 
-			// Security
-			app.use('*', (c, next) => import('hono/csrf').then(({ csrf }) => csrf()(c, next)));
-			// Allow only GET on documentation site
-			// app.use('*/docs', (c, next) =>
-			// 	import('hono/cors').then(({ cors }) =>
-			// 		cors({
-			// 			origin: '*',
-			// 			allowMethods: ['POST', 'OPTIONS'],
-			// 			maxAge: 300,
-			// 		})(c, next),
-			// 	),
-			// );
-			app.use('*', (c, next) =>
-				import('hono/cors').then(({ cors }) =>
-					cors({
-						origin: '*',
-						allowMethods: ['GET', 'OPTIONS'],
-						maxAge: 300,
-					})(c, next),
-				),
-			);
-
-			// Performance
-			app.use('*', (c, next) => import('hono/etag').then(({ etag }) => etag()(c, next)));
-
-			// Debug
-			app.use('*', (c, next) => import('hono/timing').then(({ timing }) => timing()(c, next)));
-			app.use('*', async (c, next) => {
-				if (c.env.NODE_ENV === 'development') {
-					return import('hono/logger').then(({ logger }) => logger()(c, next));
-				}
-
-				await next();
-			});
-
+		await Promise.all([import('@hono/zod-validator'), import('zod')]).then(([{ zValidator }, { z }]) =>
 			app.use(
 				'/:version/*',
 				zValidator(
@@ -71,42 +101,12 @@ export default class extends WorkerEntrypoint<EnvVars> {
 						}
 					},
 				),
-			);
+			),
+		);
 
-			// Variable Setup
-			app.use('*', async (c, next) => {
-				c.set('bodyClone', secondaryRequest);
+		await import('~/docs.mjs').then(({ default: docs }) => app.route('/:version/docs', docs));
+		await import('~/base.mjs').then(({ default: baseApp }) => app.route('/', baseApp));
 
-				await next();
-			});
-			app.use('*', async (c, next) =>
-				Promise.all([import('~shared/helpers/index.mjs'), import('~shared/db-core/db.mjs')]).then(async ([{ Helpers }, { DBManager }]) => {
-					if (Helpers.isLocal(c.env.CF_VERSION_METADATA)) {
-						await import('~shared/db-core/db.mjs').then(({ StaticDatabase }) =>
-							c.set(
-								'r_db',
-								DBManager.getDrizzle(
-									{
-										accountId: c.env.CF_ACCOUNT_ID,
-										apiToken: c.env.CF_API_TOKEN,
-										databaseId: c.env.ENVIRONMENT === 'production' ? StaticDatabase.Root.eaas_root : StaticDatabase.Root.eaas_root_p,
-									},
-									c.env.NODE_ENV !== 'production',
-								),
-							),
-						);
-					} else {
-						c.set('r_db', DBManager.getDrizzle(c.env.EAAS_ROOT, c.env.NODE_ENV !== 'production'));
-					}
-
-					await next();
-				}),
-			);
-
-			app.route('/:version/docs', docs);
-			app.route('/', baseApp);
-
-			return app.fetch(request, this.env, this.ctx);
-		});
+		return app.fetch(request, this.env, this.ctx);
 	}
 }
