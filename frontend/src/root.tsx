54d301bc775f90bf330ed028e367bbfea0586c02
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-03-04 11:35:02 -0700

    Drop `less`

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -3,7 +3,7 @@ import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.
 import { initFlowbite } from 'flowbite';
 import { RouterHead } from './components/router-head/router-head';
 
-import './global.less';
+import './global.css';
 
 export default component$(() => {
 	const nonce = useServerData<string | undefined>('nonce');
