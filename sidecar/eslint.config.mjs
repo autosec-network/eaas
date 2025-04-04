d265690d3335f8776f06d790fcf714b665ec4f90
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-04-04 12:15:21 -0700

    Reset `eslint`

---
+++ sidecar/eslint.config.mjs
@@ -0,0 +1,26 @@
+import drizzlePlugin from 'eslint-plugin-drizzle';
+import tseslint from 'typescript-eslint';
+import rootConfig from '../eslint.config.mjs';
+
+export default tseslint.config({
+	extends: [...rootConfig],
+	plugins: {
+		'@typescript-eslint': tseslint.plugin,
+		drizzle: drizzlePlugin,
+	},
+	languageOptions: {
+		parserOptions: {
+			projectService: {
+				allowDefaultProject: ['eslint.config.mjs'],
+			},
+			tsconfigRootDir: import.meta.dirname,
+			ecmaFeatures: {
+				jsx: true,
+			},
+		},
+	},
+	rules: {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+		...drizzlePlugin.configs.recommended.rules,
+	},
+});
