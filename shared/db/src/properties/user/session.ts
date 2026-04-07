61c02e0a9ec30eaedafc28b3ed95dc81ac2fae1d
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2026-04-06 22:31:10 -0700

    debug db

--- shared/db/src/properties/user/session.ts
+++ shared/db/src/properties/user/session.ts
@@ -6,6 +6,11 @@ export const SessionPropertiesSchema = zm.object({
 	lite_binding: zm.instanceof(ArrayBuffer).check(zm.refine((buf) => buf.byteLength === 512 / 8)),
 	normal_binding: zm.instanceof(ArrayBuffer).check(zm.refine((buf) => buf.byteLength === 512 / 8)),
 	sensitive_binding: zm.instanceof(ArrayBuffer).check(zm.refine((buf) => buf.byteLength === 512 / 8)),
+	binding_debug: zm.object({
+		lite: zm.array(zm.string()).check(zm.minLength(1)),
+		normal: zm.array(zm.string()).check(zm.minLength(1)),
+		sensitive: zm.array(zm.string()).check(zm.minLength(1)),
+	}),
 	// eslint-disable-next-line zod/no-any-schema
 	generated_registration_options: zm.optional(zm.record(zm.string().check(zm.trim(), zm.minLength(1)), zm.any())),
 });
@@ -15,6 +20,11 @@ export const SessionPropertiesSchema4 = z4.object({
 	lite_binding: z4.instanceof(ArrayBuffer).refine((buf) => buf.byteLength === 512 / 8),
 	normal_binding: z4.instanceof(ArrayBuffer).refine((buf) => buf.byteLength === 512 / 8),
 	sensitive_binding: z4.instanceof(ArrayBuffer).refine((buf) => buf.byteLength === 512 / 8),
+	binding_debug: z4.object({
+		lite: z4.array(z4.string()).nonempty(),
+		normal: z4.array(z4.string()).nonempty(),
+		sensitive: z4.array(z4.string()).nonempty(),
+	}),
 	// eslint-disable-next-line zod/no-any-schema
 	generated_registration_options: z4.record(z4.string().trim().nonempty(), z4.any()).optional(),
 });
